1e8845ea162536032ca1dbbaefd4fb27bf6ba2c9
Hyun Lim <hlim@lyft.com>
2016-02-09 18:54:18 -0800

    Add ui connections and initialize controller

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tipLabel.text = "$0.00"
+        self.totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
